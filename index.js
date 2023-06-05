84a6ddf1ce1f80a832705450d6f43243ecb08496
Phongphat <130993206+Phongphat-ohm@users.noreply.github.com>
2023-06-05 21:42:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,13 +68,13 @@ async function getWallet(code, phoneNumber) {
 
 app.post('/1', (req, res) => {
     const url = req.body;
-//     getWallet(url).then(result => {
-//         res.send(result)
-//     }).catch(error => {
-//         res.status(400).send("Error: " + error);
-//     });
+    getWallet(url).then(result => {
+        res.send(result)
+    }).catch(error => {
+        res.status(400).send("Error: " + error);
+    });
 //     log(url)
-    res.send(url)
+//     res.send(url)
 });
 
 // getWallet("https://gift.truemoney.com/campaign/?v=bgavSkg5hspZYKzcdk", 0994475548).then(result => {
